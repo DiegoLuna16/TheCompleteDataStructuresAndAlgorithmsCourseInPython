dc9f20fc02ad8c364add0664b240c2cc8d41d228
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-09-29 16:18:26 -0600

    how to create different types of arrays

--- arrays.py
+++ arrays.py
@@ -1,4 +1,16 @@
-import numpy as np
-arr = np.array([1, 2, 3, 4])
+import array
+
+arr = array.array('i',)
 print(arr)
+
+arr_1 = array.array('i', [1,2,3])
+print(arr_1)
+
+import numpy as np
+
+np_arr = np.array([], dtype=int)
+print(np_arr)
+
+np_arr1 = np.array([1,2,3,4], dtype=int)
+print(np_arr)
 
