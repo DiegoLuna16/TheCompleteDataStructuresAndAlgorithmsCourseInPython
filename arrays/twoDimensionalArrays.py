0f3add2da7423339d1a6541b86ed3775da1f939c
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-09-30 14:12:18 -0600

    creating two dimensional array

---
+++ arrays/twoDimensionalArrays.py
@@ -0,0 +1,8 @@
+#Day 1 - 11, 15, 10, 6
+#Day 2 - 10, 14, 11, 5
+
+import numpy as np
+
+twoDimensionalArray = np.array( [ [11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9]] )
+print(twoDimensionalArray)
+print(twoDimensionalArray[0][0])
