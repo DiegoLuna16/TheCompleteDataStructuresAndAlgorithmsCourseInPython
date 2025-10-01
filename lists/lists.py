e02d75281237883e5c3fc27d769fddf7ea1f02a2
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-01 10:14:23 -0600

    update/insert lists

--- lists/lists.py
+++ lists/lists.py
@@ -24,3 +24,19 @@ for i in range(len(ingredients)):
     ingredients[i] = ingredients[i]+"+"
 
 print(ingredients)
+
+
+#Update/Insert 
+
+myList = [1,2,3,4,5,6,7]
+print(myList)
+myList[2] = 33
+print(myList)
+myList.insert(0, 100);
+print(myList)
+myList.append(200)
+print(myList)
+
+newList = [34,54,2,3,212,32,42]
+myList.extend(newList);
+print(myList)
